9cff8606d81d88ae6032bc35ea91d145d8c2fba7
Iliyan Konakchiev <115622628+konakchiev@users.noreply.github.com>
2023-11-02 15:59:44 +0200

    update

--- register.php
+++ register.php
@@ -1,13 +1,12 @@
-<?php 
-    include('includes/autoloader.php');
-    session_start();
+<?php
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP OOP Login System</title>
+    <title>PHP OOP Login System | Register</title>
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/fontawesome-all.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/theme.css">
@@ -20,7 +19,7 @@
                 <div class="form-content">
                     <div class="form-items">
                         <div class="website-logo-inside">
-                            <a href="/php-oop-login-system-practice">
+                            <a href="/">
                                 <div class="logo">
                                     <img class="logo-size" src="images/logo-light.svg" alt="">
                                 </div>
@@ -29,13 +28,16 @@
                         <h3>Get more things done with Loggin platform.</h3>
                         <p>Access to the most powerfull tool in the entire design and web industry.</p>
                         <div class="page-links">
-                            <a href="/php-oop-login-system-practice" class="active">Login</a><a href="/php-oop-login-system-practice/register.php">Register</a>
+                            <a href="/php-oop-login-system-practice">Login</a><a href="/php-oop-login-system-practice/register.php" class="active">Register</a>
                         </div>
-                        <form action="includes/login.inc.php" method="post">
-                            <input class="form-control" type="text" name="username" placeholder="E-mail Address" required>
+                        <form>
+                            <input class="form-control" type="text" name="name" placeholder="Full Name" required>
+                            <input class="form-control" type="text" name="username" placeholder="Username" required>
+                            <input class="form-control" type="email" name="email" placeholder="E-mail Address" required>
                             <input class="form-control" type="password" name="password" placeholder="Password" required>
+                            <input class="form-control" type="password" name="repeatpassword" placeholder="Repeat Password" required>
                             <div class="form-button">
-                                <button id="submit" type="submit" name="submit" class="ibtn">Login</button>
+                                <button id="submit" type="submit" class="ibtn">Register</button>
                             </div>
                         </form>
                     </div>
@@ -43,9 +45,9 @@
             </div>
         </div>
     </div>
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/popper.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<script src="assets/js/main.js"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
 </body>
 </html>
